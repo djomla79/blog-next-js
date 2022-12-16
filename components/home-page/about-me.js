333637c90e52a987cb2052ac4a0eaab5e03acc60
Mladen Todorovic <mladen.todorovic@codecentric.de>
2022-12-16 13:26:54 +0100

    Change extension for profile picture

--- components/home-page/about-me.js
+++ components/home-page/about-me.js
@@ -7,7 +7,7 @@ function AboutMe() {
     <section className={classes.about}>
       <div className={classes.image}>
         <Image
-          src='/images/site/profile_picture.jpg'
+          src='/images/site/profile_picture.png'
           alt='An image'
           width={250}
           height={250}
